f036368aa7f2939f50a72d6bfb9972fce818be62
milagrosb02 <arimibor@hotmail.com>
2024-03-19 18:20:59 -0300

    Email por defecto de Laravel configurado

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -16,11 +19,26 @@ class AuthServiceProvider extends ServiceProvider
         //
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
+   
+
+    // este metodo se manda a llamar cada vez que el usuario presione "enviar confirmacion de email"
+    public function boot()
     {
-        //
+        $this->registerPolicies();
+
+        // llamo a la clase de verificar email y le paso dos parametros (notificacion y URL)
+        VerifyEmail::toMailUsing(function($notifiable, $url){
+
+            return (new MailMessage)
+                ->subject('Verificar Cuenta')
+                ->line('Tu cuenta ya esta casi lista, sólo debes presionar el enlace a continuación')
+
+                // Boton
+                ->action('Confirmar Cuenta', $url)
+
+                ->line('Si no creaste esta cuenta puedes ignorar este mensaje');
+
+        });
+
     }
 }
